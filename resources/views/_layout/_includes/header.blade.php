1280796e85e607b28d62e750bdf812c263f13625
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-04-15 20:55:19 -0300

    meta description

--- resources/views/_layout/_includes/header.blade.php
+++ resources/views/_layout/_includes/header.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name=”description” content=" Diga adeus às planilhas. VetPlus é um sistema voltado para a gestão de clínicas veterinárias.">
 </head>
 
 <body>
