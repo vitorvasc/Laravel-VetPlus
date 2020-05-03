cddaa0f248a6aeeed75d0c9d18ff1eca40404e44
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-05-02 23:43:38 -0300

    correção de erro ao popular formulários com dados após erro de edição

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -6,7 +6,7 @@
 
 <div class="row content list">
     @if (isset($message) || session('message'))
-    @include('_layout.error', ['message' => isset($message) ? $message :  session('message')])
+    @include('_layout.error', ['message' => isset($message) ? $message : session('message')])
     @endif
 
     <div class="col s12">
@@ -31,18 +31,19 @@
 
             <div class="input-field col l12">
                 <input id="nome" name="nome" type="text" required class="validate" maxlength="128"
-                @if(session('data')['nome']) value="{{session('data')['nome']}}" @else value="{{$cliente->nome_completo}}" @endif>
+                    @if(isset(session('data')['nome'])) value="{{session('data')['nome']}}" @else
+                    value="{{$cliente->nome_completo}}" @endif>
                 <label for="nome">Nome completo *</label>
             </div>
 
             <div class="input-field col l6">
-                <input type="text" disabled class="validate" maxlength="14"
-                @if(session('data')['cpf']) value="{{session('data')['cpf']}}" @else value="{{$cliente->cpf}}" @endif>
+                <input type="text" disabled class="validate" maxlength="14" @if(isset(session('data')['cpf']))
+                    value="{{session('data')['cpf']}}" @else value="{{$cliente->cpf}}" @endif>
                 <label for="cpf">CPF *</label>
             </div>
             <div class="input-field col l6">
-                <input id="rg" name="rg" type="text" class="validate" maxlength="16"
-                @if(session('data')['rg']) value="{{session('data')['rg']}}" @else value="{{$cliente->rg}}" @endif>
+                <input id="rg" name="rg" type="text" class="validate" maxlength="16" @if(isset(session('data')['rg']))
+                    value="{{session('data')['rg']}}" @else value="{{$cliente->rg}}" @endif>
                 <label for="rg">RG</label>
             </div>
 
@@ -70,40 +71,47 @@
 
             <div class="input-field col l2">
                 <input id="cep" name="cep" type="text" required class="validate" maxlength="9"
-                @if(session('data')['cep']) value="{{session('data')['cep']}}" @else value="{{$endereco->cep}}" @endif">
+                    @if(isset(session('data')['cep'])) value="{{session('data')['cep']}}" @else
+                    value="{{$endereco->cep}}" @endif">
                 <label for="cep">CEP *</label>
             </div>
 
             <div class="input-field col l6">
                 <input id="endereco" name="endereco" type="text" required class="validate" maxlength="255"
-                @if(session('data')['endereco']) value="{{session('data')['endereco']}}" @else value="{{$endereco->logradouro}}" @endif">
+                    @if(isset(session('data')['endereco'])) value="{{session('data')['endereco']}}" @else
+                    value="{{$endereco->logradouro}}" @endif">
                 <label for="endereco">Endereço *</label>
             </div>
 
             <div class="input-field col l2">
                 <input id="numero" name="numero" type="text" required class="validate" maxlength="8"
-                @if(session('data')['numero']) value="{{session('data')['numero']}}" @else value="{{$endereco->numero}}" @endif">
+                    @if(isset(session('data')['numero'])) value="{{session('data')['numero']}}" @else
+                    value="{{$endereco->numero}}" @endif">
                 <label for="numero">Número *</label>
             </div>
             <div class="input-field col l2">
                 <input id="complemento" name="complemento" type="text" class="validate" maxlength="16"
-                @if(session('data')['complemento']) value="{{session('data')['complemento']}}" @else value="{{$endereco->complemento}}" @endif">
+                    @if(isset(session('data')['complemento'])) value="{{session('data')['complemento']}}" @else
+                    value="{{$endereco->complemento}}" @endif">
                 <label for="complemento">Complemento</label>
             </div>
 
             <div class="input-field col s3">
                 <input id="bairro" name="bairro" type="text" required class="validate" maxlength="255"
-                @if(session('data')['bairro']) value="{{session('data')['bairro']}}" @else value="{{$endereco->bairro}}" @endif">
+                    @if(isset(session('data')['bairro'])) value="{{session('data')['bairro']}}" @else
+                    value="{{$endereco->bairro}}" @endif">
                 <label for="bairro">Bairro *</label>
             </div>
             <div class="input-field col l7">
                 <input id="cidade" name="cidade" type="text" required class="validate" maxlength="255"
-                @if(session('data')['cidade']) value="{{session('data')['cidade']}}" @else value="{{$endereco->cidade}}" @endif">
+                    @if(isset(session('data')['cidade'])) value="{{session('data')['cidade']}}" @else
+                    value="{{$endereco->cidade}}" @endif">
                 <label for="cidade">Cidade *</label>
             </div>
             <div class="input-field col s2">
                 <input id="uf" name="uf" type="text" required class="validate" maxlength="2"
-                @if(session('data')['uf']) value="{{session('data')['uf']}}" @else value="{{$endereco->uf}}" @endif">
+                    @if(isset(session('data')['uf'])) value="{{session('data')['uf']}}" @else value="{{$endereco->uf}}"
+                    @endif">
                 <label for="uf">UF *</label>
             </div>
 
@@ -134,7 +142,8 @@
 
             <div class="input-field col l6">
                 <input id="telefone" name="telefone" type="text" required class="validate" maxlength="16"
-                @if(session('data')['telefone']) value="{{session('data')['telefone']}}" @else value="{{$telefone->telefone}}" @endif">
+                    @if(isset(session('data')['telefone'])) value="{{session('data')['telefone']}}" @else
+                    value="{{$telefone->telefone}}" @endif">
                 <label for="telefone">Telefone *</label>
             </div>
 
@@ -143,12 +152,12 @@
                     <label for="whatsapp">É WhatsApp?</label>
                     <label>
                         <input class="with-gap" id="whatsapp" name="whatsapp" type="radio" value="1"
-                        @if($telefone->whatsapp) checked @endif />
+                            @if($telefone->whatsapp) checked @endif />
                         <span>Sim</span>
                     </label>
                     <label>
                         <input class="with-gap" id="whatsapp" name="whatsapp" type="radio" value="0"
-                        @if(!$telefone->whatsapp) checked @endif />
+                            @if(!$telefone->whatsapp) checked @endif />
                         <span>Não</span>
                     </label>
                 </p>
@@ -156,7 +165,8 @@
 
             <div class="input-field col l12">
                 <input id="email" name="email" type="email" required class="validate" maxlength="128"
-                @if(session('data')['email']) value="{{session('data')['email']}}" @else value="{{$email->email}}" @endif">
+                    @if(isset(session('data')['email'])) value="{{session('data')['email']}}" @else
+                    value="{{$email->email}}" @endif">
                 <label for="email">Email *</label>
             </div>
 
